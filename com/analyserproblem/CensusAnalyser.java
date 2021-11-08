31fd7d28daac4c7cd56c9f9ae275c5a127361155
Praphulla Kherade <prafullkherade9@gmail.com>
2021-11-08 19:35:37 +0530

    Welcome to Indian State Census Analyser Problem

---
+++ com/analyserproblem/CensusAnalyser.java
@@ -0,0 +1,8 @@
+package com.analyserproblem;
+
+public class CensusAnalyser {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome To Indian State Census Analyser");
+    }
+}
